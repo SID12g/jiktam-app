1d860a8b0f575445703e8790a451bec882ebd7de
조성민 <sid12g@SID-MacBook-Pro.local>
2025-08-22 21:04:34 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <iframe
+      src="https://jiktam-ui-sin.notion.site/ebd/257282c30e2580cdb26cddc13c3f4872?v=257282c30e258063b3ab000c55327d05"
+      width="100%"
+      height="100%"
+      frameborder="0"
+      allowfullscreen
+    />
+  );
+}
+
+export default App;
